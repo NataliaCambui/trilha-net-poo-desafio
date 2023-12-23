6a583e3bc53fdbc33bae894c2c772fd6561b8f9e
Natalia Cambui <natalia.lima@aluno.faculdadeimpacta.com.br>
2023-12-23 19:24:19 -0300

    desafio concluido

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -3,12 +3,17 @@ namespace DesafioPOO.Models
     public abstract class Smartphone
     {
         public string Numero { get; set; }
-        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        protected string Modelo{get;set;}
+        private string IMEI { get; set; }
+        private string Memoria { get; set; }
 
-        public Smartphone(string numero)
+        public Smartphone(string numero, string modelo, string imei, string memoria)
         {
             Numero = numero;
-            // TODO: Passar os parâmetros do construtor para as propriedades
+            Modelo = modelo;
+            IMEI = imei;
+            Memoria = memoria;
+
         }
 
         public void Ligar()
@@ -22,5 +27,6 @@ namespace DesafioPOO.Models
         }
 
         public abstract void InstalarAplicativo(string nomeApp);
+
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
-﻿using DesafioPOO.Models;
+﻿using System.ComponentModel;
+using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia");
+Smartphone Nokia = new Nokia(numero:"9999999", modelo:"Modelo1", imei:"11111111", memoria: "64");
+Nokia.Ligar();
+Nokia.ReceberLigacao();
+Nokia.InstalarAplicativo("Whatsapp");
+
+Console.WriteLine("Smartphone Iphone");
+Smartphone Iphone = new Iphone(numero: "888888880", modelo:"Modelo2", imei: "22222222", memoria: "128");
+Iphone.Ligar();
+Iphone.ReceberLigacao();
+Iphone.InstalarAplicativo("Telegram");
